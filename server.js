30ec9c7889717e15244eefe039908a584903b2ec
berkayaskin <https://github.com/berkayaskin>
2018-09-06 23:41:46 +0300

    get user profiles

--- server.js
+++ server.js
@@ -80,16 +80,17 @@ app.post('/register', (req, res) => {
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
-    let found = false;
-    database.users.forEach(user => {
-        if(user.id === id) {
-            found = true;
-            return res.json(user);
-        }
+    db.select('*').from('users').where({
+        id: id
     })
-    if (!found) {
-        res.status(400).json('user not found');
-    }
+    .then(user => {
+        if(user.length) {
+            res.json(user[0])
+        } else {
+            res.status(400).json('Not found')
+        }  
+    })
+    .catch(err => res.status(400).json('Error getting user'))
 })
 
 app.put('/image', (req, res) => {
