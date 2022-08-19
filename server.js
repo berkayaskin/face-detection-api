4cb1ea793b512fe1ba3fcf99db1120d6f45c233e
berkayaskin <berkayaskiin@gmail.com>
2022-08-19 03:53:33 +0300

    updating db config

--- server.js
+++ server.js
@@ -9,14 +9,18 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
+
 
 const db = knex({
-    client: 'pg',
-    connection: {
-        connectionString : process.env.DATABASE_URL,
-        ssl: true
-    }
-  });
+	client: "pg",
+	connection: {
+		connectionString: process.env.DATABASE_URL,
+		ssl: {
+			rejectUnauthorized: false,
+		},
+	},
+});
 
 const app = express();
 
